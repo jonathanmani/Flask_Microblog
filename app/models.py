9981693424a45ebd52237d26ea13a5d96e1aa62a
Jonathan Mani <jonathanmani@protonmail.com>
2021-02-11 10:30:18 -0500

    add hash password function in models.py

--- app/models.py
+++ app/models.py
@@ -19,3 +19,9 @@ class Post(db.Model):
 
 def __repr__(self):
     return '<Post {}>'.format(self.body)
+
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
