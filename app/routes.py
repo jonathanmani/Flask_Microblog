d044b8c64ecd00896f148b7badca2560133b00bb
Jonathan Mani <jonathanmani@protonmail.com>
2021-02-12 12:15:53 -0500

    add login function to redirect user back to the page they were previously on before login and blocked other pages until login successful

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,13 @@ from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user
 from app.models import User
+from flask_login import logout_user
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index ():
     user = {'username': 'Jonathan'}
     posts = [
@@ -30,6 +34,14 @@ def login():
         if user is None or not user.check_password(form.password.data):
                 flash('Invalid username or password')
                 return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data) 
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index') 
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+        logout_user()
+        return redirect(url_for('index'))
