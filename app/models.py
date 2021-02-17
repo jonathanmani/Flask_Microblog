9f2397566309cf1fd81728519804e545a800ccbd
Jonathan Mani <jonathanmani@protonmail.com>
2021-02-17 16:37:30 -0500

    fix functions that were incorrectly excluded from User and Post class

--- app/models.py
+++ app/models.py
@@ -10,8 +10,14 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-def __repr__(self):
-    return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -19,14 +25,9 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-def __repr__(self):
-    return '<Post {}>'.format(self.body)
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
