861c278393dcddf30849ad4603ac4eb2f061eeae
Jonathan Mani <jonathanmani@protonmail.com>
2021-02-19 15:22:11 -0500

    a few minor updates to code

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,11 @@ from app.models import User
 from werkzeug.urls import url_parse
 from datetime import datetime
 
-
+@app.before_request
+def before_request():
+        if current_user.is_authenticated:
+                current_user.last_seen = datetime.utcnow()
+                db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -74,11 +78,6 @@ def user(username):
         ]
         return render_template('user.html', user=user, posts = posts)
 
-@app.before_request
-def before_request():
-        if current_user.is_authenticated:
-                current_user.last_seen = datetime.utcnow()
-                db.session.commit()
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -92,5 +91,5 @@ def edit_profile():
                 return redirect(url_for('edit_profile'))
         elif request.method == 'GET':
                 form.username.data = current_user.username
-                form.about_me = current_user.about_me
+                form.about_me.data = current_user.about_me
         return render_template('edit_profile.html', title='Edit Profile', form=form)
