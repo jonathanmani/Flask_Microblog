c71c3d66e5f731e9bac29724143ec6ccd1ede775
Jonathan Mani <jonathanmani@protonmail.com>
2021-03-03 16:03:31 -0500

    created routes and html pages to reset user password

--- app/forms.py
+++ app/forms.py
@@ -54,3 +54,9 @@ class PostForm(FlaskForm):
 class ResetPasswordRequestForm(FlaskForm):
     email=StringField('Email', validators=[DataRequired(), Email()])
     submit=SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password=PasswordField('Password', validators=[DataRequired()])
+    password2=PasswordField(
+        'Reset Password', validators=[DataRequired(), EqualTo('password')])
+    submit=SubmitField('Request Password Reset')
